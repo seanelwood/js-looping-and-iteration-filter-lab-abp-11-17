ed2be8b9d840b525ab6985a793c381b734364b7c
Sean Elwood <seanelwood@gmail.com>
2017-11-16 23:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  return drivers.filter (function(driver) {
+    if(name.toLowerCase() === driver.toLowerCase()) {
+      return true;
+    } else {
+      return false;
+      }
+  })
+
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter (function(driver) {
+    if (name === drivers[0];) {
+      return true;
+    } else {
+      return false;
+    }
+  })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter (function(driver) {
+  if(name === driver) {
+    return true;
+  } else {
+    return false;
+    }
+  })
+}
