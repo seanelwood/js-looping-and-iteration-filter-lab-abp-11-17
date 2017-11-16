d699f60ae69089e6a1ca4eb9524e8d87777e2d8b
Sean Elwood <seanelwood@gmail.com>
2017-11-16 23:08:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ function findMatching (drivers, name) {
 
 }
 
-function fuzzyMatch(drivers, name) {
-  return drivers.filter (function(driver) {
-    if (name === drivers[0];) {
-      return true;
-    } else {
-      return false;
-    }
-  })
-}
-
-function matchName(drivers, name) {
-  return drivers.filter (function(driver) {
-  if(name === driver) {
-    return true;
-  } else {
-    return false;
-    }
-  })
-}
+// function fuzzyMatch(drivers, name) {
+//   return drivers.filter (function(driver) {
+//     if (name === drivers[0];) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   })
+// }
+// 
+// function matchName(drivers, name) {
+//   return drivers.filter (function(driver) {
+//   if(name === driver) {
+//     return true;
+//   } else {
+//     return false;
+//     }
+//   })
+// }
